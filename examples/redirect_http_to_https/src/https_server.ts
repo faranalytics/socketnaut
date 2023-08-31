7f0f55e7eb0ed1c4441aae39e6f67ab08b080468
adpatter <adpatter@umich.edu>
2023-08-31 19:57:47 -0400

    more

--- examples/redirect_http_to_https/src/https_server.ts
+++ examples/redirect_http_to_https/src/https_server.ts
@@ -1,13 +1,15 @@
 import * as http from 'node:http';
 import * as https from 'node:https';
 import * as fs from 'fs';
+import * as os from 'os';
+import * as pth from 'path';
 import { Level, createServiceAgent } from 'socketnaut';
 
 const service = createServiceAgent({
     server: https.createServer(
         {
-            key: fs.readFileSync('../../../secrets/key.pem'),
-            cert: fs.readFileSync('../../../secrets/crt.pem')
+            key: fs.readFileSync(pth.resolve(os.homedir(), 'secrets/key.pem')),
+            cert: fs.readFileSync(pth.resolve(os.homedir(), 'secrets/crt.pem'))
         }) // Configure this HTTPS server however you choose.
 });
 
@@ -15,7 +17,7 @@ service.logHandler.setLevel(Level.DEBUG)
 
 service.server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
     for (let now = Date.now(), then = now + 100; now < then; now = Date.now()); // Block for 100 milliseconds.
-    res.end('Hello World!');
+    req.pipe(res);
 });
 
 service.server.listen({ port: 0, host: '127.0.0.1' });
