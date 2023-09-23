330d9326ea421206d38d0d593825f8bcc8f9ca25
adpatter <adpatter@umich.edu>
2023-09-22 21:07:13 -0400

    Improve example.

--- examples/socketnaut_koa/http_server.js
+++ examples/socketnaut_koa/http_server.js
@@ -3,7 +3,7 @@ import { createServiceAgent, Level } from 'socketnaut';
 
 const app = new Koa();
 
-const server = app.listen({ port: 0, host: '127.0.0.1' })
+const server = app.listen({ port: 0, host: '127.0.0.1' });
 // Specifying port 0 here will instruct the Server to listen on a random port.  
 // The Socketnaut Agent will communicate the randomly selected port to the ServiceProxy.
 
