76c59912ab02d73c9b1ade76b9826994c9f9a0ba
adpatter <adpatter@umich.edu>
2023-12-30 14:27:14 -0500

    Improve naming.

--- tests/test/src/service.ts
+++ tests/test/src/service.ts
@@ -1,20 +1,20 @@
 import * as http from 'node:http';
 import { Level, createServiceAgent, ProxySocketAddressInfo } from 'socketnaut';
 
-const service = createServiceAgent({
+const agent = createServiceAgent({
     server: http.createServer()
 });
 
-service.log.setLevel(Level.DEBUG);
+agent.log.setLevel(Level.DEBUG);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-service.server.on('request', async (req: http.IncomingMessage, res: http.ServerResponse) => {
+agent.server.on('request', async (req: http.IncomingMessage, res: http.ServerResponse) => {
     for (let now = Date.now(), then = now + 100; now < then; now = Date.now()); // Block for 100 milliseconds.
-    const proxyAddressInfo: ProxySocketAddressInfo = await service.requestProxySocketAddressInfo(req.socket);
+    const proxyAddressInfo: ProxySocketAddressInfo = await agent.requestProxySocketAddressInfo(req.socket);
     console.log(proxyAddressInfo);
     req.pipe(res);
 });
 
-service.server.listen({ port: 0, host: '127.0.0.1' });
+agent.server.listen({ port: 0, host: '127.0.0.1' });
 // Specifying port 0 here will cause the Server to listen on a random port.
 // Socketnaut will communicate the random port number to the ServiceProxy.
