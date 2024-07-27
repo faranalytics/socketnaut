1192ba9dd28df4de2f162a3b21efe767d892abe7
adamjpatterson <adam@farar.net>
2024-07-27 15:15:52 -0400

    Add linting config.

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    { ignores: ['dist', 'examples/use_streams_in_a_node_project'] },
+    {
+        files: ['**/*.ts'],
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: true,
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-floating-promises": ["error"],
+            "@typescript-eslint/no-misused-promises": ["error"],
+            'semi': 'error',
+            'quotes': ['error', 'single']
+        },
+    });
