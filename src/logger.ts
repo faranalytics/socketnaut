1642b00ab1c596b136525f0b0c610dedac0d81a9
adamjpatterson <adam@farar.net>
2024-07-28 14:02:09 -0400

    Add logger configuration.

---
+++ src/logger.ts
@@ -0,0 +1,19 @@
+import {
+    Logger,
+    Formatter,
+    ConsoleHandler,
+    SyslogLevel,
+} from 'streams-logger';
+
+export const logger = new Logger({ level: SyslogLevel.INFO, captureStackTrace: false, parent: null });
+export const formatter = new Formatter({
+    format: async ({ level, isotime, hostname, pid, message, }) => (
+        `<${level}> ${isotime} ${hostname} ${pid} - ${message}\n`
+    )
+});
+export const consoleHandler = new ConsoleHandler({ level: SyslogLevel.INFO });
+export const log = logger.connect(
+    formatter.connect(
+        consoleHandler
+    )
+);
