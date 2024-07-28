80d0b3d345beca2e8ec8ad6ec06a59b552bc7d96
adamjpatterson <adam@farar.net>
2024-07-28 14:02:35 -0400

    Add logger exports.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import * as threads from 'node:worker_threads';
 import { createServiceAgent, ServiceAgent, ServiceAgentOptions } from './service_agent.js';
 import { createServiceProxy, ServiceProxy, ServiceProxyOptions } from './service_proxy.js';
 import { WorkerAgent } from './worker_agent.js';
-import { ConsoleHandler, Metadata, LevelLogger, MetadataFormatter, Level } from 'memoir';
 import { ProxySocketAddressInfo } from './types.js';
+import { SyslogLevel as Level } from 'streams-logger';
+import { log, logger, formatter, consoleHandler } from './logger.js';
 
 export {
     createServiceProxy,
@@ -14,22 +15,14 @@ export {
     ServiceAgentOptions,
     ProxySocketAddressInfo,
     WorkerAgent,
-    Level
+    Level,
+    logger,
+    formatter,
+    consoleHandler,
+    log
 };
 
 if (threads.isMainThread) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const formatter = (message: string, { name, level, func, url, line, col }: Metadata): string =>
-        `${level}:${new Date().toISOString()}:${name}:${func}:${line}:${col}:${message}`;
-
-    const log = new LevelLogger<string, string>({ name: 'socketnaut', level: Level.INFO });
-    const consoleHandler = new ConsoleHandler<string, string>();
-    const metadataFormatter = new MetadataFormatter<string, string>({ formatter });
-
-    consoleHandler.setLevel(Level.DEBUG);
-    consoleHandler.setFormatter(metadataFormatter);
-    log.addHandler(consoleHandler);
-
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
     const { version } = require('../package.json');
     log.info?.(`socketnaut v${version}`);
