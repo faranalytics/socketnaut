94016e9ab701c36c7a5600a873d35049ad14a8bb
adamjpatterson <adam@farar.net>
2024-12-27 17:35:39 -0500

    Improve examples.

--- examples/hello_world_tsx/index.ts
+++ examples/hello_world_tsx/index.ts
@@ -1,14 +1,15 @@
 import * as net from 'node:net';
 import { createServiceProxy, Level } from 'socketnaut';
 
-const server = net.createServer(); // Configure this TCP Server however you choose.
+const server : net.Server= net.createServer(); // Configure this TCP Server however you choose.
 
 server.listen({ port: 3080, host: '0.0.0.0' });
 
 const proxy = createServiceProxy({
     server,
     workerCount: 42,
-    workerURL: './http_server.js'
+    workerURL: `import { tsImport } from "tsx/esm/api"; await tsImport("./http_server.ts", import.meta.url);`,
+    workerOptions: { eval: true }
 });
 
 proxy.log.setLevel(Level.DEBUG);
