754141829872f3e4286e70a6a3d644dcb38241d3
adamjpatterson <adam@farar.net>
2024-12-28 13:04:27 -0500

    Improve tests.

--- tests/test/src/index.ts
+++ tests/test/src/index.ts
@@ -32,19 +32,19 @@ await describe('A suite of tests.', async () => {
     let httpsProxy: ChildProcess;
 
     before(async () => {
-        log.info('Starting HTTP and HTTPS proxies.');
+        log.info('Starting 2 HTTP proxies and 100 HTTPS proxies.');
         httpProxy = fork('./dist/http_proxy.js', process.argv.slice(2));
         httpsProxy = fork('./dist/https_proxy.js', process.argv.slice(2));
         await Promise.all([listen(httpProxy, 'ready'), listen(httpsProxy, 'ready')]);
-        log.info('Started HTTP and HTTPS proxies.');
+        log.info('Started 2 HTTP proxies and 100 HTTPS proxies.');
     });
 
     after(async () => {
-        log.info('Stopping HTTP and HTTPS proxies.');
+        log.info('Stopping 2 HTTP proxies and 100 HTTPS proxies.');
         httpsProxy.send('exit');
         httpProxy.send('exit');
         await Promise.all([once(httpsProxy, 'exit'), once(httpProxy, 'exit')]);
-        log.info('Stopped HTTP and HTTPS proxies.');
+        log.info('Stopped 2 HTTP proxies and 100 HTTPS proxies.');
     });
 
     void test('Make 1000 requests each with a message body of 100,000 bytes.', async (t) => {
