7b83668cd2f1f967471fbd900581fa3fc69af195
adamjpatterson <adam@farar.net>
2025-01-13 23:26:49 -0500

    Refactor WorkerAgent.

--- src/worker_agent.ts
+++ src/worker_agent.ts
@@ -1,5 +1,6 @@
 import * as threads from 'node:worker_threads';
 import * as net from 'node:net';
+import { once } from 'node:events';
 import { Agent } from 'port_agent';
 
 interface WorkerAgentOptions {
@@ -20,29 +21,16 @@ export class WorkerAgent extends Agent {
         this.worker = worker;
         this.connections = 0;
 
-        this.socketConnectOptsReady = new Promise<boolean>((r, e) => {
-            worker.once('exit', e);
-            worker.once('error', e);
-
-            worker.once('online', async () => {
-                try {
-                    const socketConnectOpts = await this.call<net.SocketConnectOpts>('socketConnectOpts');
-                    if (socketConnectOpts) {
-                        this.socketConnectOpts = socketConnectOpts;
-                    }
-                    else {
-                        e('The Worker came online; however, it is missing a socketConnectOpts.');
-                    }
-
-                    worker.removeListener('exit', e);
-                    worker.removeListener('error', e);
-
-                    r(true);
-                }
-                catch (err) {
-                    e(err);
-                }
-            });
-        });
+        this.socketConnectOptsReady = (async () => {
+            await once(worker, 'online');
+            const socketConnectOpts = await this.call<net.SocketConnectOpts>('socketConnectOpts');
+            if (socketConnectOpts) {
+                this.socketConnectOpts = socketConnectOpts;
+            }
+            else {
+                throw new Error('The Worker came online; however, it is missing a socketConnectOpts.');
+            }
+            return true;
+        })();
     }
 }
