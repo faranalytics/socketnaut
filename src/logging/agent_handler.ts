98d6a097bd84e57c4c41b6704dade2ceed066e46
Adam Patterson <adam@farar.net>
2025-10-31 22:56:00 -0400

    Format and improve error handling.

--- src/logging/agent_handler.ts
+++ src/logging/agent_handler.ts
@@ -1,30 +1,29 @@
-import * as stream from 'node:stream';
-import { Agent } from 'port_agent';
-import { Node, LogContext, SyslogLevelT, Config } from 'streams-logger';
+import * as stream from "node:stream";
+import { Agent } from "port_agent";
+import { Node, LogContext, SyslogLevelT, Config } from "streams-logger";
 
 export class AgentHandler extends Node<LogContext<string, SyslogLevelT>, never> {
-    constructor(agent: Agent, callable: string, streamOptions?: stream.WritableOptions) {
-        super(
-            new stream.Writable({
-                ...Config.getWritableOptions(true),
-                ...streamOptions,
-                ...{
-                    objectMode: true,
-                    write: async (
-                        chunk: LogContext<string, SyslogLevelT>,
-                        encoding: BufferEncoding,
-                        callback: stream.TransformCallback
-                    ) => {
-                        try {
-                            await agent.call(callable, chunk);
-                            callback();
-                        }
-                        catch (err) {
-                            callback(err instanceof Error ? err : new Error('Error'));
-                        }
-                    },
-                },
-            })
-        );
-    }
+  constructor(agent: Agent, callable: string, streamOptions?: stream.WritableOptions) {
+    super(
+      new stream.Writable({
+        ...Config.getWritableOptions(true),
+        ...streamOptions,
+        ...{
+          objectMode: true,
+          write: (
+            chunk: LogContext<string, SyslogLevelT>,
+            encoding: BufferEncoding,
+            callback: stream.TransformCallback
+          ) => {
+            (async () => {
+              await agent.call(callable, chunk);
+              callback();
+            })().catch((reason: unknown) => {
+              callback(reason instanceof Error ? reason : new Error(String(reason)));
+            });
+          },
+        },
+      })
+    );
+  }
 }
