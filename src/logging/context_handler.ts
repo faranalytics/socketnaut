b97bc7ac9d4591b62a09568cf20a74c8a0272c67
Adam Patterson <adam@farar.net>
2025-10-31 22:58:17 -0400

    Format and auto-bind method.

--- src/logging/context_handler.ts
+++ src/logging/context_handler.ts
@@ -1,26 +1,28 @@
-import * as stream from 'node:stream';
-import { Node, LogContext, SyslogLevelT, Config, LogContextOptions} from 'streams-logger';
+import * as stream from "node:stream";
+import { Node, LogContext, SyslogLevelT, Config, LogContextOptions } from "streams-logger";
 
 export class ContextHandler extends Node<LogContext<string, SyslogLevelT>, LogContext<string, SyslogLevelT>> {
-    constructor(streamOptions?: stream.TransformOptions) {
-        super(new stream.PassThrough({
-            ...Config.getDuplexOptions(true, true),
-            ...streamOptions, ...{
-                readableObjectMode: true,
-                writableObjectMode: true
-            }
-        }));
-    }
+  constructor(streamOptions?: stream.TransformOptions) {
+    super(
+      new stream.PassThrough({
+        ...Config.getDuplexOptions(true, true),
+        ...streamOptions,
+        ...{
+          readableObjectMode: true,
+          writableObjectMode: true,
+        },
+      })
+    );
+  }
 
-    public async write(logContextOptions: LogContextOptions<string, SyslogLevelT>): Promise<void> {
-        try {
-            const logContext = new LogContext<string, SyslogLevelT>(logContextOptions);
-            await super._write(logContext);
-        }
-        catch (err) {
-            if (err instanceof Error) {
-                Config.errorHandler(err);
-            }
-        }
+  public write = async (logContextOptions: LogContextOptions): Promise<void> => {
+    try {
+      const logContext = new LogContext<string, SyslogLevelT>(logContextOptions);
+      await super._write(logContext);
+    } catch (err) {
+      if (err instanceof Error) {
+        Config.errorHandler(err);
+      }
     }
+  };
 }
