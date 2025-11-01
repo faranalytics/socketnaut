3e8aebac71492174c2599216ac3e7d6b24a04704
Adam Patterson <adam@farar.net>
2025-10-31 22:53:53 -0400

    Format and improve typing.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
-import * as threads from 'node:worker_threads';
-import { createServiceAgent, ServiceAgent, ServiceAgentOptions } from './service_agent.js';
-import { createServiceProxy, ServiceProxy, ServiceProxyOptions } from './service_proxy.js';
-import { WorkerAgent } from './worker_agent.js';
-import { ProxySocketAddressInfo } from './types.js';
-import { SyslogLevel, SyslogLevel as Level } from 'streams-logger';
-import { log, logger, formatter, consoleHandler } from './logging/logger.js';
+import * as threads from "node:worker_threads";
+import { createServiceAgent, ServiceAgent, ServiceAgentOptions } from "./service_agent.js";
+import { createServiceProxy, ServiceProxy, ServiceProxyOptions } from "./service_proxy.js";
+import { WorkerAgent } from "./worker_agent.js";
+import { ProxySocketAddressInfo } from "./types.js";
+import { SyslogLevel, SyslogLevel as Level } from "streams-logger";
+import { log, logger, formatter, consoleHandler } from "./logging/logger.js";
 
 export {
-    createServiceProxy,
-    ServiceProxy,
-    ServiceProxyOptions,
-    createServiceAgent,
-    ServiceAgent,
-    ServiceAgentOptions,
-    ProxySocketAddressInfo,
-    WorkerAgent,
-    SyslogLevel,
-    Level,
-    logger,
-    formatter,
-    consoleHandler,
-    log
+  createServiceProxy,
+  ServiceProxy,
+  ServiceProxyOptions,
+  createServiceAgent,
+  ServiceAgent,
+  ServiceAgentOptions,
+  ProxySocketAddressInfo,
+  WorkerAgent,
+  SyslogLevel,
+  Level,
+  logger,
+  formatter,
+  consoleHandler,
+  log,
 };
 
 if (threads.isMainThread) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
-    const { version } = require('../package.json');
-    log.info(`socketnaut v${version}`);
-    log.info(`pid ${process.pid}`);
+  const { version } = require("../package.json") as { version: string };
+  log.info(`socketnaut v${version}`);
+  log.info(`pid ${process.pid.toString()}`);
 }
