9195b9487a9ca8dad9720c5f8c4eec9feb694d39
Adam Patterson <adam@farar.net>
2025-10-31 23:04:48 -0400

    Format and improve error handling.

--- src/worker_agent.ts
+++ src/worker_agent.ts
@@ -1,34 +1,25 @@
-import * as threads from 'node:worker_threads';
-import * as net from 'node:net';
-import { once } from 'node:events';
-import { Agent } from 'port_agent';
+import * as threads from "node:worker_threads";
+import * as net from "node:net";
+import { once } from "node:events";
+import { Agent } from "port_agent";
 
 interface WorkerAgentOptions {
-    worker: threads.Worker;
+  worker: threads.Worker;
 }
 
 export class WorkerAgent extends Agent {
-    public worker: threads.Worker;
-    public connections: number;
-    public socketConnectOpts?: net.SocketConnectOpts;
-    public socketConnectOptsReady: Promise<boolean>;
+  public worker: threads.Worker;
+  public connections: number;
+  public socketConnectOpts: Promise<net.SocketConnectOpts | null>;
 
-    constructor({ worker }: WorkerAgentOptions
-    ) {
-        super(worker);
-        this.worker = worker;
-        this.connections = 0;
+  constructor({ worker }: WorkerAgentOptions) {
+    super(worker);
+    this.worker = worker;
+    this.connections = 0;
 
-        this.socketConnectOptsReady = (async () => {
-            await once(worker, 'online');
-            const socketConnectOpts = await this.call<net.SocketConnectOpts>('socketConnectOpts');
-            if (socketConnectOpts) {
-                this.socketConnectOpts = socketConnectOpts;
-            }
-            else {
-                throw new Error('The Worker came online; however, it is missing a socketConnectOpts.');
-            }
-            return true;
-        })();
-    }
+    this.socketConnectOpts = (async () => {
+      await once(worker, "online");
+      return await this.call<net.SocketConnectOpts | null>("socketConnectOpts");
+    })().catch(() => null);
+  }
 }
