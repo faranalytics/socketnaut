3761e5c01cfe02bab6bdd7d4456a10210311ac74
Adam Patterson <adam@farar.net>
2025-10-31 22:59:26 -0400

    Improve logging.

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,17 +1,12 @@
-import { Logger, Formatter, ConsoleHandler, SyslogLevel } from 'streams-logger';
-import { ContextHandler } from './context_handler';
+import { Logger, Formatter, ConsoleHandler, SyslogLevel } from "streams-logger";
+import { ContextHandler } from "./context_handler";
 
 export const logger = new Logger({ level: SyslogLevel.INFO, captureStackTrace: false, parent: null });
 export const formatter = new Formatter({
-    format: async ({ level, isotime, hostname, pid, message, }) => (
-        `<${level}> ${isotime} ${hostname} ${pid} - ${message}\n`
-    )
+  format: ({ level, isotime, hostname, pid, message }) =>
+    `<${level}> ${isotime ?? ""} ${hostname ?? ""} ${pid?.toString() ?? ""} - ${message}\n`,
 });
 export const consoleHandler = new ConsoleHandler({ level: SyslogLevel.DEBUG });
-export const log = logger.connect(
-    formatter.connect(
-        consoleHandler
-    )
-);
+export const log = logger.connect(formatter.connect(consoleHandler));
 export const contextHandler = new ContextHandler();
 contextHandler.connect(log);
